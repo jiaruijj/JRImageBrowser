0abf3ee4e0e3c6456b49a1274afc49446f868c1a
JR <jiaruijj@163.com>
2016-08-11 23:24:25 +0800

    添加资源文件

---
+++ JRImageBrowser/JRImageBrowser/FNTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FNTableViewController.h
+//  FNFullImageDemo
+//
+//  Created by JR on 16/8/10.
+//  Copyright © 2016年 JR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FNTableViewController : UITableViewController
+
+@end
