1fc83abc93b389176627a21700d804e78a10e102
JR <jiaruijj@163.com>
2016-08-11 22:50:14 +0800

    添加工程

---
+++ JRImageBrowser/JRImageBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRImageBrowser
+//
+//  Created by JR on 16/8/11.
+//  Copyright © 2016年 JR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JRImageBrowser/JRImageBrowser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JRImageBrowser
+//
+//  Created by JR on 16/8/11.
+//  Copyright © 2016年 JR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
